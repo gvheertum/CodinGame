b6fc6105365b7ee399ea5bdab236c3247d340b7a
Gertjan van Heertum <git@netietsmeer.nl>
2017-10-22 12:05:57 +0200

    Defib can read defib data from inputlines

--- Puzzles/13_ Defibrillators.cs
+++ Puzzles/13_ Defibrillators.cs
@@ -24,17 +24,47 @@ namespace Puzzles.Defibrillators
 			new DefibrillatorsSolution(new CodingGameProxyEngine()).Run();
 		}
 
+		public class DefibData
+		{
+			public int Number { get; set; }
+			public string Name { get; set; }
+			public string Address { get; set; }
+			public string Phone { get; set; }
+			public string Longitude { get; set; }
+			public string Latitude { get; set; }
+			public override string ToString()
+			{
+				return $"{Name}: {Longitude}-{Latitude}";
+			}
+		}
+
 		public override void Run()
 		{
 			string longitude = ReadLine();
 			string latitude = ReadLine();
+			List<DefibData> defibs = new List<DefibData>();
+
 			int nrOfDefibs = int.Parse(ReadLine());
 			for (int i = 0; i < nrOfDefibs; i++)
 			{
-				string DEFIB = ReadLine();
+				defibs.Add(GetDefibFromString(ReadLine()));
 			}
-
+			defibs.ForEach(d => Log(d));
 			WriteLine("answer");
+		}
+
+		private DefibData GetDefibFromString(string input)
+		{
+			var spl = input.Split(";");
+			return new DefibData()
+			{
+				Number = Int32.Parse(spl[0]),
+				Name = spl[1],
+				Address = spl[2],
+				Phone = spl[3],
+				Longitude = spl[4],
+				Latitude = spl[5]
+			};
 		}
 	}
 }
