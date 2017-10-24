6b2a6dc433519e55dbcb621542414e7d33ad43dd
Gertjan van Heertum <git@netietsmeer.nl>
2017-10-24 16:35:48 +0200

    Skynet puzzle -> set up

---
+++ Puzzles/14_Skynet.cs
@@ -0,0 +1,116 @@
+//https://www.codingame.com/ide/puzzle/skynet-revolution-episode-1
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Shared;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+namespace Puzzles.Skynet
+{
+	public class SkynetPlayer : Shared.PuzzleMain
+	{
+		protected SkynetPlayer(IGameEngine gameEngine) : base(gameEngine)
+		{
+		}
+
+		static void Main(string[] args)
+		{
+			new SkynetPlayer(new CodingGameProxyEngine()).Run();
+		}
+
+		public class Node
+		{
+			public int NodeIndex { get; set; }
+			public List<Node> LinkedNodes { get; set; } = new List<Node>();
+			public List<int> LinkedNodesIndex { get; set; } = new List<int>();
+			public bool IsExitNode { get; set; }
+		}
+
+		public override void Run()
+		{
+
+			string[] inputs;
+			inputs = ReadLine().Split(' ');
+			
+			//Initialize nodes
+			int nrOfNodes = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+			var nodes = CreateNodes(nrOfNodes).ToList();
+			var nodeDictionary = CreateNodeDictionary(nodes);
+			
+			//Link the nodes
+			int nrOfLinks = int.Parse(inputs[1]); // the number of links
+			LinkNodes(nodeDictionary, nrOfLinks);
+			
+			//Flag exits
+			int nrOfExitGates = int.Parse(inputs[2]); // the number of exit gateways
+			FlagExitNodes(nodeDictionary, nrOfExitGates);
+			
+			
+			
+
+			// game loop
+			while (IsRunning())
+			{
+				int SI = int.Parse(ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
+
+				// Write an action using WriteLine()
+				// To debug: Error.WriteLine("Debug messages...");
+
+
+				// Example: 0 1 are the indices of the nodes you wish to sever the link between
+				WriteLine("0 1");
+			}
+		}
+
+		private IEnumerable<Node> CreateNodes(int nodeCount)
+		{
+			for(int i = 0; i < nodeCount; i++)
+			{ 
+				yield return new Node() { NodeIndex = i };
+			}
+		}
+
+		private Dictionary<int, Node> CreateNodeDictionary(List<Node> nodes)
+		{
+			Dictionary<int, Node> dict = new Dictionary<int, Node>();
+			nodes.ForEach(n => dict.Add(n.NodeIndex, n));
+			return dict;
+		}
+
+		private void LinkNodes(Dictionary<int, Node> nodes, int nrOfLinks)
+		{
+			for (int i = 0; i < nrOfLinks; i++)
+			{
+				var inputs = ReadLine().Split(' ');
+				int nodeId1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+				int nodeId2 = int.Parse(inputs[1]);
+
+				Log($"Linking: {nodeId1} <-> {nodeId2}");
+				var node1 = nodes[nodeId1];
+				var node2 = nodes[nodeId2];
+				node1.LinkedNodes.Add(node2);
+				node1.LinkedNodesIndex.Add(node2.NodeIndex);
+
+				node2.LinkedNodes.Add(node1);
+				node2.LinkedNodesIndex.Add(node1.NodeIndex);
+			}
+		}
+
+		private void FlagExitNodes(Dictionary<int, Node> nodes, int nrOfExitGates)
+		{
+			for (int i = 0; i < nrOfExitGates; i++)
+			{
+				int nodeId = int.Parse(ReadLine()); // the index of a gateway node
+				Log($"Node {nodeId} -> Exit");
+				nodes[nodeId].IsExitNode = true;
+			}
+		}
+	}
+}
