c75b0982c664111c9d8ea0380bbc1acbc6987075
Gertjan van Heertum <git@netietsmeer.nl>
2018-01-02 14:03:08 +0100

    Updated the stock puzzle to work with delta points instead of brute-forcing, and this seems to fix it :D

--- Puzzles/17_StockLosses.cs
+++ Puzzles/17_StockLosses.cs
@@ -4,18 +4,27 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
-using Shared;
 using Framework;
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
- //https://www.codingame.com/ide/puzzle/don't-panic-episode-1
- //https://www.codingame.com/training/hard/don't-panic-episode-2
+ //https://www.codingame.com/ide/puzzle/stock-exchange-losses
+
 
 namespace Puzzles.StockLosses
 {
+	public class StockRange
+	{
+		public int Highest { get; set; }
+		public int Lowest { get; set; }
+		public int Diff { get { return Highest-Lowest; } }
+		public override string ToString()
+		{
+			return $"{Highest} - {Lowest} -> {Diff}";
+		}
+	}
 	public class StockLossesPuzzle : PuzzleMain
 	{
 		protected StockLossesPuzzle(IGameEngine gameEngine) : base(gameEngine)
@@ -31,27 +40,44 @@ namespace Puzzles.StockLosses
 		{
 			int n = int.Parse(ReadLine());
 			string[] inputs = ReadLine().Split(' ');
-			List<int> stockValues = new List<int>();
+			List<StockRange> ranges = new List<StockRange>();
+
 			for (int i = 0; i < n; i++)
 			{
-				int v = int.Parse(inputs[i]);
-				stockValues.Add(v);
-				//Console.Error.WriteLine($"Found: {v}");
+				int value = int.Parse(inputs[i]);
+				if(IsNewHigh(value, ranges))
+				{
+					var newRange = new StockRange() { Highest = value, Lowest = value };
+					ranges.Add(newRange);
+				} //Since the value is a new high we are sure this will not need to update the lowest for the others (doesnt make sense)
+				else
+				{
+					UpdateLowestInRanges(value, ranges);
+				}
 			}
 
 			// Write an action using Console.WriteLine()
 			// To debug: Console.Error.WriteLine("Debug messages...");
-			int lowestDrop = GetLargestLoss(stockValues);
-			Log($"Largest loss: {lowestDrop}");
-			if(lowestDrop > 0) 
-			{ 
-				Log("Loss was positive, so make it 0");
-				lowestDrop = 0; 
-			}
-			Console.WriteLine(lowestDrop);
-			
+			Log($"Found {ranges.Count} delta points");
+			var rangeToUse = ranges.OrderByDescending(r => r.Diff).First();
+			Console.WriteLine(-rangeToUse.Diff);	
 		}
-		static int GetLargestLoss(List<int> values)
+
+		//Check if there is a new high point found, this will create a new range item
+		private bool IsNewHigh(int value, List<StockRange> ranges)
+		{
+			if(!ranges.Any()) { return true; }
+			return ranges.All(r => r.Highest < value); 
+		}
+
+		//Check which ranges need to be updated to the newest low point (we can not pick any lowest, since if the highest is after the lowpoint the delta is not correct)
+		private void UpdateLowestInRanges(int value, List<StockRange> ranges)
+		{
+			ranges.Where(r => r.Lowest > value).ToList().ForEach(r => r.Lowest = value);
+		}
+
+		//Old legacy function for brutforcing algorithm
+		private int GetLargestLoss(List<int> values)
 		{
 			List<int> lowestDropPerItem = new List<int>();
 			for(int i = 0; i < values.Count; i++)
