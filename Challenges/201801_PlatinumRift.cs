e6a9e4aea6dd0a30239569b2437716f7a607181c
Gertjan van Heertum <git@netietsmeer.nl>
2018-01-27 14:13:47 +0100

    Expanding game object for reading status

--- Challenges/201801_PlatinumRift.cs
+++ Challenges/201801_PlatinumRift.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using Framework;
+using Shared;
 
 //https://www.codingame.com/ide/puzzle/platinum-rift-episode-1
 namespace Challenges.PlatinumRift
@@ -26,8 +27,65 @@ namespace Challenges.PlatinumRift
 				[Player] id={MyPlayerID}, platinum={MyPlatinum}"
 			.Replace("\t","");
 		}	
+
+		public Dictionary<int,RiftZone> Zones {get;set;} = new Dictionary<int, RiftZone>();
+		public void AddOrReplaceZone(RiftZone zone)
+		{
+			if(Zones.Keys.Contains(zone.NodeIndex))
+			{
+				Zones[zone.NodeIndex] = zone;
+			}
+			else
+			{
+				Zones.Add(zone.NodeIndex, zone);
+			}
+		}
+
+		public RiftZone GetZone(int zoneId)
+		{
+			if(!Zones.Keys.Contains(zoneId)) { throw new Exception($"No zone with id: {zoneId}"); }
+			return Zones[zoneId];
+		}
+
+		public List<RiftZoneLink> ZoneLinks {get;set;} = new List<RiftZoneLink>();
 	}
 
+	public class RiftZone : Node<RiftZone>
+	{
+		public int PlatimumSource {get;set;}
+		public int? OwningPlayerID {get;set;}
+		public int AmountPodsPlayer0 {get;set;}
+		public int AmountPodsPlayer1 {get;set;}
+		public int AmountPodsPlayer2 {get;set;}
+		public int AmountPodsPlayer3 {get;set;}
+		public int GetAmountPodsForPlayer(int playerID)
+		{
+			switch(playerID)
+			{
+				case 0: return AmountPodsPlayer0;
+				case 1: return AmountPodsPlayer1;
+				case 2: return AmountPodsPlayer2;
+				case 3: return AmountPodsPlayer3;
+				default: throw new Exception($"Unknown playerID {playerID}");
+			}
+		}
+		public void SetAmountPodsForPlayer(int playerID, int pods)
+		{
+			switch(playerID)
+			{
+				case 0: AmountPodsPlayer0 = pods; break;
+				case 1: AmountPodsPlayer1 = pods; break;;
+				case 2: AmountPodsPlayer2 = pods; break;;
+				case 3: AmountPodsPlayer3 = pods; break;;
+				default: throw new Exception($"Unknown playerID {playerID}");
+			}
+		}
+	}
+
+	public class RiftZoneLink : NodeRoute<RiftZone>
+	{
+
+	}
 
 	public class PlatinumRiftPlayer : PuzzleMain
 	{
@@ -87,9 +145,15 @@ namespace Challenges.PlatinumRift
 			for (int i = 0; i < game.AmountOfZones; i++)
 			{
 				inputs = ReadLine().Split(' ');
-				int zoneId = int.Parse(inputs[0]); // this zone's ID (between 0 and zoneCount-1)
-				int platinumSource = int.Parse(inputs[1]); // the amount of Platinum this zone can provide per game turn
+				RiftZone rz = new RiftZone()
+				{
+					NodeIndex = int.Parse(inputs[0]),
+					PlatimumSource = int.Parse(inputs[1])
+				};
+				game.AddOrReplaceZone(rz);
 			}
+
+			//TODO: Add routes
 			for (int i = 0; i < game.AmountOfZoneLinks; i++)
 			{
 				inputs = ReadLine().Split(' ');
@@ -106,15 +170,26 @@ namespace Challenges.PlatinumRift
 		{
 			Log("Updating gamestate");
 			gameState.MyPlatinum = int.Parse(ReadLine()); // my available Platinum
+			Log($"Current platinum: {gameState.MyPlatinum}");
+
+			//Update all the zones
 			for (int i = 0; i < gameState.AmountOfZones; i++)
 			{
 				string[] inputs = ReadLine().Split(' ');
 				int zId = int.Parse(inputs[0]); // this zone's ID
-				int ownerId = int.Parse(inputs[1]); // the player who owns this zone (-1 otherwise)
-				int podsP0 = int.Parse(inputs[2]); // player 0's PODs on this zone
-				int podsP1 = int.Parse(inputs[3]); // player 1's PODs on this zone
-				int podsP2 = int.Parse(inputs[4]); // player 2's PODs on this zone (always 0 for a two player game)
-				int podsP3 = int.Parse(inputs[5]); // player 3's PODs on this zone (always 0 for a two or three player game)
+				var zone = gameState.GetZone(zId);
+				int? oldOwner = zone.OwningPlayerID;
+				
+				zone.OwningPlayerID = int.Parse(inputs[1]); // the player who owns this zone (-1 otherwise)
+				zone.AmountPodsPlayer0 = int.Parse(inputs[2]); // player 0's PODs on this zone
+				zone.AmountPodsPlayer1 = int.Parse(inputs[3]); // player 1's PODs on this zone
+				zone.AmountPodsPlayer2 = int.Parse(inputs[4]); // player 2's PODs on this zone (always 0 for a two player game)
+				zone.AmountPodsPlayer3 = int.Parse(inputs[5]); // player 3's PODs on this zone (always 0 for a two or three player game)
+				
+				if(zone.OwningPlayerID != oldOwner)
+				{
+					Log($"Ownership of zone {zone.NodeIndex} changed from {oldOwner} to {zone.OwningPlayerID}");
+				}
 			}
 			return gameState;
 		}
