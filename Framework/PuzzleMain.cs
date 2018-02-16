5844549e81e9676fd33865423554b4afe451f235
Gertjan van Heertum <git@netietsmeer.nl>
2018-02-16 17:33:36 +0100

    VS code did not save one of the files before commit :(

--- Framework/PuzzleMain.cs
+++ Framework/PuzzleMain.cs
@@ -81,17 +81,4 @@ namespace Framework
 			}
 		}
 	}
-
-	public abstract class LogInjectableClass
-	{
-		private Action<object> _log;
-		public LogInjectableClass(Action<object> log)
-		{
-			_log = log;
-		}
-		protected virtual void Log(object o) 
-		{
-			if(_log != null) { _log($"[State: {o}]"); }
-		}	
-	}
 }
