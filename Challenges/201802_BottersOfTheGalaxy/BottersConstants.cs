bc9d9a8aa79c329da3756dd76a29690bc990606f
Gertjan van Heertum <git@netietsmeer.nl>
2018-03-10 20:22:57 +0100

    Focussing on Hulk, the algorithm seems to be best for him

--- Challenges/201802_BottersOfTheGalaxy/BottersConstants.cs
+++ Challenges/201802_BottersOfTheGalaxy/BottersConstants.cs
@@ -34,9 +34,9 @@ namespace Challenges.BottersOfTheGalaxy
 
 			public static IEnumerable<string> PreferredHeroes()
 			{
-				yield return Deadpool;
+				//yield return Deadpool;
 				//yield return Valkery;
-				yield return DoctorStrange;
+				//yield return DoctorStrange;
 				yield return Hulk;
 				//We no like Ironman, loses too much
 				//yield return Ironman;
